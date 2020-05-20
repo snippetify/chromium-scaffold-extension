35579844950669ec38681ad5967b2f1def623d1c
Vensy <pierrevensy@gmail.com>
2020-05-19 22:30:08 -0400

    Page modal implemented

--- src/js/pagemodal.js
+++ src/js/pagemodal.js
@@ -5,7 +5,8 @@ import {
     CS_CLOSE_MODAL,
     CREATE_NEW_TAB,
     CS_MODAL_TARGET,
-    SNIPPETIFY_API_TOKEN
+    SNIPPETIFY_API_TOKEN,
+    SNIPPETIFY_API_URL
 } from './contants'
 
 /**
@@ -15,29 +16,41 @@ import {
 */
 class PageModal {
     // Properties
-    vm
     simpleMde
     codeMirror
+    payload = {}
 
     constructor () {
         $(document).ready(() => {
             if ($('#app').length > 0) {
                 this.createCodeMirrorInstance()
                 this.createSimpleMdeInstance()
-                this.registerEventListeners()
+                this.localEventListeners()
             }
         })
 
-        this.showModalEventListener()
+        this.remoteEventListener()
     }
 
-    showModalEventListener () {
+    /**
+     * Listen for remote native event.
+     * @returns void
+    */
+    remoteEventListener () {
         chrome.runtime.onMessage.addListener(e => {
             if (e.target === CS_MODAL_TARGET && e.type === CS_OPEN_MODAL) {
                 chrome.storage.local.get(SNIPPETIFY_API_TOKEN, data => {
                     const token = data[SNIPPETIFY_API_TOKEN]
+                    $('.modal').modal('hide')
                     if (token) {
-                        $('#snippetForm').modal('show')
+                        this.hydrateForm(e.payload) // Hydrate form
+                        // Reset element state
+                        $('#snippetForm #save').removeClass('d-none')
+                        $('#snippetForm .done-body').addClass('d-none')
+                        $('#snippetForm .form-body').removeClass('d-none')
+                        $('#snippetForm #save .saving').addClass('d-none')
+                        $('#snippetForm #save .normal').removeClass('d-none')
+                        $('#snippetForm').modal('show') // Show modal
                     } else {
                         $('#unauthenticatedModal').modal('show')
                     }
@@ -46,18 +59,29 @@ class PageModal {
         })
     }
 
-    registerEventListeners () {
+    /**
+     * Listen for locale dom event.
+     * @returns void
+    */
+    localEventListeners () {
         // On modal hidden
-        $('#snippetForm, #unauthenticatedModal').on('hidden.bs.modal', function (e) {
+        $('#snippetForm, #unauthenticatedModal').on('hidden.bs.modal', () => {
             chrome.runtime.sendMessage({ target: CS_TARGET, type: CS_CLOSE_MODAL })
         })
 
         // On Save clicked
         $('#snippetForm').on('click', '#save', e => {
+            this.saveSnippet()
             return false
         })
 
-        // Connect now
+        // Field focus and blur
+        // Validate form
+        $('#snippetForm input.form-control').on('blur', e => {
+            this.validateField($(e.target).val(), e.target)
+        })
+
+        // Connect now, redirect to snippetify
         $('#app').on('click', '#connectNow', e => {
             chrome.runtime.sendMessage({ target: CS_TARGET, type: CS_CLOSE_MODAL })
             chrome.runtime.sendMessage({ target: BG_TARGET, type: CREATE_NEW_TAB, payload: 'https://snippetify.com/connect' })
@@ -65,33 +89,140 @@ class PageModal {
         })
     }
 
+    /**
+     * Create an instance of codemirror.
+     * @returns void
+    */
     createCodeMirrorInstance () {
         this.codeMirror = CodeMirror.fromTextArea($('#snippetForm #code')[0], {
             lineNumbers: true
         })
     }
 
+    /**
+     * Create an instance of simpleMde.
+     * @returns void
+    */
     createSimpleMdeInstance () {
         this.simpleMde = new SimpleMDE({
             forceSync: true,
             placeholder: 'Add a description',
-            element: $('#snippetForm #desc')[0]
+            element: $('#snippetForm #description')[0]
         })
     }
 
-    hydrateModalForm (payload) {
-        const self = this
-        this.vm.title = payload.title
-        this.vm.tags = payload.tags.join(', ')
-        this.codeMirror.setValue(payload.code)
-        this.codeMirror.setOption('mode', { name: payload.tags[0] })
-        this.codeMirror.on('change', function () {
-            self.vm.code = self.codeMirror.getValue()
+    /**
+     * Save snippet.
+     * Make http request to snippetify.
+     * @returns void
+    */
+    saveSnippet () {
+        if (this.validateForm(true)) return
+        $('#snippetForm #save').attr('disabled', true)
+        $('#snippetForm #save .normal').addClass('d-none')
+        $('#snippetForm #save .saving').removeClass('d-none')
+        chrome.storage.local.get(SNIPPETIFY_API_TOKEN, data => {
+            const token = data[SNIPPETIFY_API_TOKEN]
+            if (token) {
+                $.ajax({
+                    method: 'POST',
+                    data: JSON.stringify(this.payload),
+                    url: `${SNIPPETIFY_API_URL}/snippets`,
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    crossDomain: true,
+                    processData: false,
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${token}`
+                    }
+                }).done(res => {
+                    $('#snippetForm #save').addClass('d-none')
+                    $('#snippetForm .form-body').addClass('d-none')
+                    $('#snippetForm .done-body').removeClass('d-none')
+                }).fail((xhr, status) => {
+                    this.printServerErrors(xhr)
+                }).always(() => {
+                    $('#snippetForm #save').attr('disabled', false)
+                    $('#snippetForm #save .saving').addClass('d-none')
+                    $('#snippetForm #save .normal').removeClass('d-none')
+                })
+            }
         })
+    }
+
+    /**
+     * Add data to dom input element.
+     * @returns void
+    */
+    hydrateForm (payload) {
+        this.payload = payload
         this.simpleMde.value(payload.desc)
-        this.simpleMde.codemirror.on('change', function () {
-            self.vm.desc = self.simpleMde.value()
+        $('form #title').val(payload.title)
+        $('form #tags').val(payload.tags.join(', '))
+        this.codeMirror.setOption('mode', { name: payload.tags[0] })
+        this.codeMirror.setValue($('<div>').html(payload.code).text())
+        this.codeMirror.on('change', () => {
+            this.payload.code = this.codeMirror.getValue()
+            this.validateField(this.payload.code, $('#snippetForm #code')[0])
         })
+        this.simpleMde.codemirror.on('change', () => {
+            this.payload.description = this.simpleMde.value()
+            this.validateField(this.payload.description, $('#snippetForm #description')[0])
+        })
+        $('form-control').removeClass('is-invalid') // Reset for validation
+        $('#snippetForm #save').attr('disabled', this.validateForm()) // Enable or disable save btn
+    }
+
+    /**
+     * Single field validation.
+     * @returns void
+    */
+    validateField (val, el) {
+        if (val.trim().length < 2) $(el).addClass('is-invalid')
+        else $(el).removeClass('is-invalid')
+        $('#snippetForm #save').attr('disabled', this.validateForm())
+    }
+
+    /**
+     * Form validation.
+     * @returns boolean
+    */
+    validateForm (feedback) {
+        let hasError = false
+
+        $('input.form-control').each((_, el) => {
+            if ($(el).val().trim().length < 2) {
+                hasError = true
+                if (feedback) $(el).addClass('is-invalid')
+            } else if (feedback) {
+                $(el).removeClass('is-invalid')
+            }
+        })
+
+        if ((this.payload.code || '').length < 2) {
+            hasError = true
+            $('#snippetForm #code').addClass('is-invalid')
+        }
+
+        if ((this.payload.description || '').length < 2) {
+            hasError = true
+            $('#snippetForm #description').addClass('is-invalid')
+        }
+
+        return hasError
+    }
+
+    /**
+     * Print server error.
+     * @returns void
+    */
+    printServerErrors (e) {
+        if (e.status === 422) {
+            $.each(e.responseJSON.errors, (i, v) => {
+                $(`#snippetForm #${i}`).addClass('is-invalid')
+            })
+        }
     }
 }
 
